806ff3ef7530fcdd55199a28ad0f3e5087f40041
Etasha2345 <71700664+Etasha2345@users.noreply.github.com>
2020-12-16 17:43:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+menu_list_array = ["Chicken Tandoori pizza",
+    "Veg Supreme Pizza",
+    "Paneer Tikka pizza",
+    "Deluxe veggie pizza",
+    "Veg Extravaganza Pizza"
+];
+
+function getmenu() {
+    var htmldata;
+    htmldata = "<ol class='menulist'>"
+    menu_list_array.sort();
+    for (var i = 0; 1 < menu_list_array.leagth; i++) {
+        htmldata = htmldata + '<li>' + menu_list_array[i] + '</li>'
+    }
+    htmldata = htmldata + '<ol>'
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
